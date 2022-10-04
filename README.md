a8cb0f0dd2b0b4c6f0bbaf0c65e59acf50263a0b
R.S. Maodi <silasmaodi@gmail.com>
2022-10-04 14:07:07 -0700

    My Answers

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Practicing the String Calculator Kata
 7. Start from step 3
 
 ## String Calculator Kata from: http://osherove.com/tdd-kata-1/
+//tdd buddy
 
 1. Create a simple String calculator with a method int Add(string numbers)
 2. The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,59 @@
-class Calculator{
-    add(numbers){
+//Return 0 if string is empty
+console.log("\nReturn 0 if string is empty: ");
+add1(1);
+add1(0);
+add1("");
 
+console.log("\nReturn 1 if string is not empty: ");
+add2(1);
+add2(0);
+add2("");
+
+console.log("\nReturn sum: ");
+sun("1,2");
+sun("67,20");
+
+function add1(numbers){
+
+    let emptyString = "";
+    let result;
+
+    if (numbers === emptyString) {
+        result = 0;
     }
+    else{
+        result = "Not a string";
+    }
+    console.log(result);
 }
 
-module.exports = Calculator
+function add2(numbers){
+
+    let emptyString = "";
+    let result;
+
+    if (numbers != emptyString) {
+        result = 1;
+    }
+    else{
+        result = "The string is empty";
+    }
+    console.log(result);
+}
+
+
+//The function for the two string numbers
+function sun(numbers){
+
+    let emptyString = "";
+    let result = 0;
+    let myArray = numbers.split(",");
+
+    if (numbers != emptyString) {
+        result = parseInt(myArray[0]) + parseInt(myArray[1]);
+    }
+    else{
+        result = 0;
+    }
+    console.log(result);
+}
